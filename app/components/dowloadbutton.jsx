2359ae02096fcb6cd256e4fac38ef1f9aeae1c26
example <team14457@gmail.com>
2024-08-19 19:27:16 +0700

    edit 2

--- app/components/dowloadbutton.jsx
+++ app/components/dowloadbutton.jsx
@@ -6,7 +6,7 @@ import PageTransition from './PageTransition'
 export const DownloadButton = () => {
   return (
     <PageTransition>
-      <a href="https://drive.google.com/file/d/12rBJPMSuXbTfSZN0o2G_4BSNtKa-Js1O/view?usp=sharing" download target="_blank" rel="noopener noreferrer" className="flex items-center bg-fourth lg:w-full xl:w-48 p-2 mt-3 rounded-lg shadow-xl"> 
+      <a href="https://drive.google.com/file/d/1LSAKzRZKR4r_joozEUyQPlYicHQLRTmB/view?usp=drive_link" download target="_blank" rel="noopener noreferrer" className="flex items-center bg-fourth lg:w-full xl:w-48 p-2 mt-3 rounded-lg shadow-xl"> 
             <span className='mx-auto '>Download Resume</span>
             <FontAwesomeIcon icon={faDownload} className='text-secondary'/>
       </a>
